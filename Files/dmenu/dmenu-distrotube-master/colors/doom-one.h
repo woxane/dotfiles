ab1d5161755817bce70713facd8a31a2b2285953
mohsen <mohsen.minavand1384@gmail.com>
2022-04-27 20:15:30 +0430

    add necessary files

---
+++ Files/dmenu/dmenu-distrotube-master/colors/doom-one.h
@@ -0,0 +1,10 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#cccccc", "#282c34" },
+	[SchemeSel] = { "#1c1f24", "#c678dd" },
+	[SchemeSelHighlight] = { "#f1fa8c", "#c678dd" },
+	[SchemeNormHighlight] = { "#f1fa8c", "#282c34" },
+	[SchemeOut] = { "#000000", "#51afef" },
+	[SchemeOutHighlight] = { "#f1fa8c", "#51afef" },
+/*	[SchemeMid] = { "#d7d7d7", "#1c1f24" },*/
+};
